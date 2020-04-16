1c00c71f6602215f09ac9183fc213b663ea10c7e
nadadahab <nada.dahab@espace.com.eg>
2020-04-16 17:35:08 +0200

    try to swagger generate api docs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,12 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+// app/Http/Controllers/Controller.php
+/**
+ * @OA\Info(title="Online Store API", version="1.0.0")
+ * @OA\PathItem(path="/api")
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
